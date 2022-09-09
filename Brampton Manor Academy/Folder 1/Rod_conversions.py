b5b28bc1097e220e92345e57679db02506a4d560
Yasar06 <108301089+Yasar06@users.noreply.github.com>
2022-09-09 12:55:01 +0100

    Add files via upload

--- Brampton Manor Academy/Folder 1/Rod_conversions.py
+++ Brampton Manor Academy/Folder 1/Rod_conversions.py
@@ -3,43 +3,41 @@ def rod_conversion():
     print("You have entered",rods, "rods")
     return rods
 
-rods = rod_conversion()
-
 def meters_conversion(rods):
     meters = rods * 5.0292
     return meters
 
-meters = meters_conversion(rods)
 def conversion_miles(meters):
     miles = meters / 1609.34
     return miles
 
-miles = conversion_miles(meters)
 
 def conversion_furlongs(rods):
     furlongs = rods / 40
     return furlongs
 
-furlongs = conversion_furlongs(rods)
 
 def conversion_feet(meters):
     feet = meters / 0.3048
     return feet
 
-feet = conversion_feet(meters)
-
 def conversion_walking_speed(miles):
     walking_speed = miles/3.1
     return walking_speed
 
-walking_speed = conversion_walking_speed(miles)
-
 def main():
+    rods = rod_conversion()
+    meters = meters_conversion(rods)
+    miles = conversion_miles(meters)
+    furlongs = conversion_furlongs(rods)
+    feet = conversion_feet(meters)
+    walking_speed = conversion_walking_speed(miles)
+    
     print("Conversions:")
-    print("Meters:",meters)
-    print("Feet:",feet)
-    print("Miles:",miles)
-    print("Furlongs:",furlongs)
-    print("Minutes to walk:",walking_speed*60)
+    print(f"Meters: {meters}")
+    print(f"Feet: {feet}")
+    print(f"Miles: {miles}")
+    print(f"Furlongs: {furlongs}")
+    print(f"Minutes to walk: {walking_speed*60}")
 
 main()
